05035130da1d99c4f600c7bc7e77bd53af099fec
Dpsika <124811128+Dpsika@users.noreply.github.com>
2023-05-12 01:13:12 +0530

    cough content

--- src/components/heart.js
+++ src/components/heart.js
@@ -21,13 +21,13 @@ export default function Heart(){
                         </div>
                         <div className=" fadeInUp  col-sm-12 col-lg-6 col-md-6 wow align-items-center">
                             <div className="title-box wow fadeInUp text-left">
-                                <h2 style={{color:'#fff',fontSize:'42px',lineHeight:'45px',fontFamily:'Arial, Helvetica, sans-serif'}}>Heart Attack awareness</h2>
-                                <p style={{color:'#fff',fontSize:'18px',fontFamily:'Arial, Helvetica, sans-serif'}}>Every year on September 29, World Heart Day raises awareness about heart health.
-                                 In India, heart attack-related deaths have consistently exceeded 25,000 in the last four years, with over 28,000 in the past three years, as reported by 
-                                 the National Crime Records Bureau (NCRB) in their "Accidental Deaths & Suicides in India" (ADSI) data.
-                                  Cardiovascular diseases (CVD) pose a significant risk to human health and are the primary cause of death globally.<br/>&nbsp;&nbsp;&nbsp;&nbsp;
-                                  Our AI algorithm can accurately differentiate not only between normal and abnormal conditions but also identify specific, potentially life-threatening cardiac issues.
-                                   This allows for the detection of warning signals well before the disease progresses to a stage that adversely affects health outcomes.</p>
+                                <h2 style={{color:'#fff',fontSize:'42px',lineHeight:'45px',fontFamily:'Arial, Helvetica, sans-serif'}}>Detection of diseases from Cough Sound</h2>
+                                <p style={{color:'#fff',fontSize:'18px',fontFamily:'Arial, Helvetica, sans-serif'}}>Cough is a common symptom of many respiratory diseases, including COVID-19, and has been identified 
+                                as a potential indicator of infection. Artificial Intelligence (AI) is being used to analyze cough sounds to detect and diagnose diseases. By using machine learning algorithms, 
+                                AI systems can detect specific acoustic patterns in coughs that are associated with different respiratory diseases. The technology has the potential to provide a rapid, non-invasive,
+                                 and cost-effective method of disease screening, particularly in low-resource settings.<br/>&nbsp;&nbsp;&nbsp;&nbsp;
+                                 AI-powered cough detection systems can be integrated into mobile applications, wearable devices, and even smart speakers, making it accessible to a wide range of users. As a result,
+                                  cough analysis using AI has emerged as a promising tool for early disease detection and prevention.</p>
                             </div>
                            
                         </div>
